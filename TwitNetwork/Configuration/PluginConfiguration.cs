c747b46e918c997be6c9df39ae1afec3c3e6d7a8
Simon Lee <simon.lee@simon-lee.me.uk>
2020-01-06 01:23:47 +0000

    Clean copy of PluginConfiguration.cs

---
+++ TwitNetwork/Configuration/PluginConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using MediaBrowser.Model.Plugins;
+using System;
+
+namespace TwitNetwork.Configuration
+{
+    /// <summary>
+    /// Class PluginConfiguration
+    /// </summary>
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        /// <summary>
+        /// The twit.tv API key 
+        /// </summary>
+        public string AppID { get; set; }
+
+        /// <summary>
+        /// The twit.tv API Secret
+        /// </summary>
+        public string AppKey { get; set; }
+
+        /// <summary>
+        /// Enable debug logging
+        /// </summary>
+        public bool EnableDebugLogging { get; set; }
+
+        public DateTime ShowListUpdated { get; set; }
+
+        public int LimitRequestsPerMinute { get; set; }
+
+        public int LimitCollectionInDays { get; set; }
+
+        public PluginConfiguration()
+        {
+            AppID = "app-id-goes-here";
+            AppKey = "app-key-goes-here";
+            EnableDebugLogging = false;
+            LimitRequestsPerMinute = 5;
+            LimitCollectionInDays = 30;
+        }
+
+    }
+}
